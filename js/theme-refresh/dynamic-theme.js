60732da27f753d60ed5577c1439b54b52427f5c8
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-05-31 09:56:10 +0200

    Iniziata piccola modifica per impostare il tema col bottoncino

--- js/theme-refresh/dynamic-theme.js
+++ js/theme-refresh/dynamic-theme.js
@@ -1,6 +1,18 @@
+let temaAttuale = "dark"
+
 function setPreferredTheme(){
-    const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+    const tema = document.getElementById("html").getAttribute("data-bs-theme");
+    const isDark = () => tema == "dark";
     const html = document.getElementById("html");
 
-    isDark ? html.setAttribute("data-bs-theme", "dark") : html.setAttribute("data-bs-theme", "light");
+    isDark() ? html.setAttribute("data-bs-theme", "dark") : html.setAttribute("data-bs-theme", "light");
+}
+
+function setTheme(mode){
+    const html = document.getElementById("html");
+    const btnTema = document.getElementById("btnTema");
+    if (mode == "dark" || mode == "light"){
+        html.setAttribute("data-bs-theme", mode);
+    }
+    btnTema
 }
