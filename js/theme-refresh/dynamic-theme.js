c14ab792b4748044fafc443b074e1fd416b1c4d7
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-05-31 12:00:03 +0200

    Ripristinato tema preferito con JS

--- js/theme-refresh/dynamic-theme.js
+++ js/theme-refresh/dynamic-theme.js
@@ -1,18 +1,6 @@
-let temaAttuale = "dark"
-
 function setPreferredTheme(){
-    const tema = document.getElementById("html").getAttribute("data-bs-theme");
-    const isDark = () => tema == "dark";
+    const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
     const html = document.getElementById("html");
 
-    isDark() ? html.setAttribute("data-bs-theme", "dark") : html.setAttribute("data-bs-theme", "light");
-}
-
-function setTheme(mode){
-    const html = document.getElementById("html");
-    const btnTema = document.getElementById("btnTema");
-    if (mode == "dark" || mode == "light"){
-        html.setAttribute("data-bs-theme", mode);
-    }
-    btnTema
+    isDark ? html.setAttribute("data-bs-theme", "dark") : html.setAttribute("data-bs-theme", "light");
 }
